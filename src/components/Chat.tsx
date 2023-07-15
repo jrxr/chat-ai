2cebbb0d6ba1bd07ff2177a80c7a23899890ff6a
agacy junior <juniormartinssom@gmail.com>
2023-07-15 14:25:31 -0300

    fix: height adjustment

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -29,7 +29,7 @@ export function Chat() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <ScrollArea className="h-[600px] w-full pr-4">
+        <ScrollArea className="h-[550px] w-full pr-4">
           {messages.map((message) => {
             return (
               <div
